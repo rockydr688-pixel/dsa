e742992d9a46b157b1cf8a3d311066e3b882f8a4
rockydr688-pixel <rockydr688@gmail.com>
2026-07-15 09:35:16 +0530

    Time: 0 ms (100.00%) | Memory: 71.7 MB (52.57%) - LeetSync

---
+++ 53-maximum-subarray/maximum-subarray.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int maxi=nums[0],curr=nums[0];
+        for(int i=1;i<nums.size();i++){
+            curr=max(nums[i],curr+nums[i]);
+            maxi=max(maxi,curr);
+        }
+        return maxi;
+        
+    }
+};

d7c606b60032be20784fd391f980116afe9c2eb5
rockydr688-pixel <rockydr688@gmail.com>
2026-07-22 12:23:44 +0530

    Time: 3 ms (54.95%) | Memory: 32.7 MB (39.56%) - LeetSync

--- 966-binary-subarrays-with-sum/binary-subarrays-with-sum.cpp
+++ 966-binary-subarrays-with-sum/binary-subarrays-with-sum.cpp
@@ -4,9 +4,9 @@ int solve(vector<int>& nums, int goal){
      int l=0,r=0,count=0,sum=0;
         if(goal<0) return 0;
         while(r<nums.size()){
-            sum=sum+nums[r];
+            sum+=nums[r]%2;
             while(sum>goal){
-                sum-=nums[l];
+                sum-=nums[l]%2;
                 l++;
             }
             count+=r-l+1;
@@ -15,8 +15,8 @@ int solve(vector<int>& nums, int goal){
         return count;
 
 }
-    int numSubarraysWithSum(vector<int>& nums, int goal) {
-        return solve(nums,goal)-solve(nums,goal-1);
+    int numSubarraysWithSum(vector<int>& nums, int k) {
+        return solve(nums,k)-solve(nums,k-1);
        
     }
 };
